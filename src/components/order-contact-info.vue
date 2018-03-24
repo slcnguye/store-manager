8582d21d7d983dca1c580a4904b1df0997a8c8c7
Sang Nguyen <slcnguye@gmail.com>
2018-03-24 18:28:11 -0400

    Add phone number component.

--- src/components/order-contact-info.vue
+++ src/components/order-contact-info.vue
@@ -6,26 +6,27 @@
     </div>
 
     <el-dialog title="Contact Info" :visible.sync="dialogFormVisible">
+      <div class="text-extra-large padding-bottom"> {{ number | toPhoneNumber }} </div>
       <div class="text-center padding-bottom">
-        <el-button class="number-button">1</el-button>
-        <el-button class="number-button">2</el-button>
-        <el-button class="number-button">3</el-button>
+        <el-button class="number-button" @click="addDigit(1)">1</el-button>
+        <el-button class="number-button" @click="addDigit(2)">2</el-button>
+        <el-button class="number-button" @click="addDigit(3)">3</el-button>
       </div>
       <div class="text-center padding-bottom">
-        <el-button class="number-button">4</el-button>
-        <el-button class="number-button">5</el-button>
-        <el-button class="number-button">6</el-button>
+        <el-button class="number-button" @click="addDigit(4)">4</el-button>
+        <el-button class="number-button" @click="addDigit(5)">5</el-button>
+        <el-button class="number-button" @click="addDigit(6)">6</el-button>
       </div>
       <div class="text-center padding-bottom">
-        <el-button class="number-button">7</el-button>
-        <el-button class="number-button">8</el-button>
-        <el-button class="number-button">9</el-button>
+        <el-button class="number-button" @click="addDigit(7)">7</el-button>
+        <el-button class="number-button" @click="addDigit(8)">8</el-button>
+        <el-button class="number-button" @click="addDigit(9)">9</el-button>
       </div>
       <div class="text-center">
-        <el-button type="warning" plain round class="number-button" @click="dialogFormVisible = false">
+        <el-button type="warning" plain round class="number-button" @click="revertDigit">
           <i class="fa fa-undo fa-lg"></i>
         </el-button>
-        <el-button class="number-button">0</el-button>
+        <el-button class="number-button" @click="addDigit(0)">0</el-button>
         <el-button type="success" plain round class="number-button" @click="dialogFormVisible = false">
           <i class="fa fa-check fa-lg"></i>
         </el-button>
@@ -41,17 +42,17 @@ export default {
   data: function () {
     return {
       dialogFormVisible: false,
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
-      formLabelWidth: '120px'
+      number: ''
+    }
+  },
+  methods: {
+    addDigit (digit) {
+      if (this.number.length < 10) {
+        this.number += digit
+      }
+    },
+    revertDigit () {
+      this.number = this.number.slice(0, -1)
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/hello-world'
 import HomePage from '@/components/home-page'
 import Documentation from '@/components/documentation'
 import Order from '@/components/order'
@@ -14,11 +13,6 @@ export default new Router({
       path: '/',
       name: 'home-page',
       component: HomePage
-    },
-    {
-      path: '/hello-world',
-      name: 'hello-world',
-      component: HelloWorld
     },
     {
       path: '/order',
