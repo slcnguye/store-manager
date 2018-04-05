b4e675d69f3f2c758c82eea1dd980640dc67e1b8
Sang Nguyen <slcnguye@gmail.com>
2018-04-05 19:28:45 -0400

    Add basic landing page to display store name and address

---
+++ src/components/landing-page.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="homepage">
+    <el-container>
+      <el-header>
+      </el-header>
+      <el-main>
+        Home page
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      activeIndex: 'home',
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
